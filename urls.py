7b2b22066c4efc5f2b5e13bfdbfbc971c2f5d7db
chaitrahp <42713346+chaitrahp@users.noreply.github.com>
2019-04-22 14:14:48 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,22 +1,26 @@
-from django.urls import path
-from accounts import views
-from django.contrib.auth.views import LoginView,LogoutView
-from django.contrib.auth import views as auth_views
+"""tutorial URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+#from django.conf.urls import url, include
+from tutorial import views
 
 urlpatterns = [
-    path('',views.home),
-    path('login/',auth_views.LoginView.as_view(template_name= 'accounts/login.html'),name="login"),
-    path('logout/',auth_views.LogoutView.as_view(template_name='accounts/logout.html'),name="logout"),
-    path('register/',views.register,name='register'),
-    path('profile/',views.view_profile,name='view_profile'),
-    path('profile/edit/',views.edit_profile,name='edit_profile'),
-    path('change-password/',views.change_password,name='change_profile'),
-    path('contact/',views.contactus,name='contact'),
-    path('action/',views.action,name='action'),
-    path('education/',views.education,name='education'),
-    path('fiction/',views.fiction,name='fiction'),
-    path('history/',views.history,name='history'),
-    path('drama/',views.drama,name='drama'),
-    path('cart/',views.cart,name='cart'),
-    
+    path('',views.login_redirect, name='login_redirect'),
+    path('admin/', admin.site.urls),
+    path('accounts/',include('accounts.urls')),
+    path('register/',views.register,name='register')
 ]
--- views.py
+++ views.py
@@ -1,118 +1,19 @@
+from django.shortcuts import redirect, HttpResponse
 
-from django.shortcuts import render,redirect
-from django.shortcuts import HttpResponse
-#from django.contrib.auth.forms import UserCreationForm
-from accounts.forms import(
-UserCreationForm,
- RegistrationForm,
- EditProfileForm,
- contactform
- )
-#from django.http.response import HttpResponseNotAllowed
-#from . forms import register
-from django.contrib.auth.models import User
-
-from .models import category,category1,category2,category3,category4
-#from .models import category
-from django.contrib.auth.forms import UserChangeForm,PasswordChangeForm
-from django.contrib.auth import update_session_auth_hash
-from django.contrib.auth.decorators import login_required
-
-from django.shortcuts import get_object_or_404
-# Create your views here.
-
-def action(request):
-    act=get_object_or_404(category)
-    return render(request,'accounts/action.html',{'act':act})
-
-def education(request):
-    edu=get_object_or_404(category1)
-    return render(request,'accounts/education.html',{'edu':edu})
-
-
-def drama(request):
-    drama=get_object_or_404(category2)
-    return render(request,'accounts/dramabooks.html',{'drama':drama})
-
-
-def fiction(request):
-    fic=get_object_or_404(category3)
-    return render(request,'accounts/fictionbooks.html',{'fic':fic})
-
-
-def history(request):
-    his=get_object_or_404(category4)
-    return render(request,'accounts/history.html',{'his':his})
-
-def cart(request):
-    args={'user':request.user}
-    return render(request,'accounts/cart.html',args)
-
-
-def home(request):
-
-	numbers=[1,2,3,4,5]
-	name='abcd'
-	args={'myname':name,'numbers':numbers}
-    #cats=category.objects.all 
-	return render(request,'accounts/home.html',args)
-    #return render(request,'accounts/home.html',{'cats':cats})
+def login_redirect(request):
+    return redirect('/accounts/login')
 
 def register(request):
-	if request.method=="POST":
-		form=RegistrationForm(request.POST)
-		#return response('Hi')
-		
-		if form.is_valid():
-			form.save()
-			#return HttpResponseRedirect('app1/home.html')
-			return redirect('/accounts')
-			#return render(request,'app1/home.html')
-
-		else:
-			return HttpResponseNotAllowed(['get','post'])
-
-
-	else:
-		form=RegistrationForm()
-
-		args = {'form':form}
-	return render(request, 'accounts/reg_form.html',args) 
-
-@login_required
-def view_profile(request):
-    args={'user':request.user}
-    return render(request,'accounts/profile.html',args)
-
-def edit_profile(request):
-    if request.method == 'POST':
-        form=EditProfileForm(request.POST,instance=request.user)
+    if request.method=="POST":
+        form=UserCreationForm(request.POST)
 
         if form.is_valid():
             form.save()
-            return redirect('/accounts/profile')
-
-    else:
-        form=EditProfileForm(instance=request.user)
-        args={'form':form}
-        return render(request,'accounts/edit_profile.html',args)
-
- 
-def change_password(request):
-    if request.method == 'POST':
-        form=PasswordChangeForm(data=request.POST,user=request.user)
-
-        if form.is_valid():
-            form.save()
-            update_session_auth_hash(request,form.user)
-            return redirect('/accounts/profile')
         else:
-            return redirect('/accounts/change-password')
-    else:
-        form=PasswordChangeForm(user=request.user)
-        args={'form':form}
-        return render(request,'accounts/change_password.html',args)
+            return render(request,'accounts/home.html',{'form':form})
 
-def contactus(request):
-    conform=contactform()
-    return render(request,'accounts/contact.html',{'conform':conform})
+    else:
+        form=UserCreationForm()
+
+        args={'form':form}
+        return render(request, 'accounts/reg_form.html',args)
